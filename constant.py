5624acef18b0fa484fbad821c55c0193eac6a0e2
Xavier <xavier.chen2000@gmail.com>
2021-05-25 15:04:42 +0800

    changed windows prefix

--- constant.py
+++ constant.py
@@ -6,7 +6,7 @@ path_prefix = '.'
 chrome_user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36"
 
 if 'nt' in os.name:
-    path_prefix = os.sep.join(('D:', 'Program Files (x86)', 'Investing-Python', 'Investing-Scrape'))
+    path_prefix = os.sep.join(('D:', 'Program Files (x86)', 'Investing-Python', 'IScrape'))
     chrome_user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36"
 
 cf_clearance_json_path = os.sep.join((path_prefix, 'local', 'cf_clearance.json'))
